a0ba7b426e024c12f55e2e9eace4f6131f86cc4d
pratikmtele <pratiktele4@gmail.com>
2025-07-29 11:34:23 +0530

    Updated Menu Animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
 
   return (
     <header
-      className={`flex justify-between h-20 items-center sm:mt-6 z-50 w-full px-5 lg:mt-0 lg:fixed lg:w-full lg:px-[160px] lg:lgm-auto lg:top-0 transition-colors duration-300 shadow-md ${
+      className={`flex justify-between h-20 items-center sm:mt-6 z-50 w-full px-5 lg:mt-0 lg:fixed lg:w-full lg:px-[18.4%] lg:lgm-auto lg:top-0 transition-colors duration-300 shadow-md ${
         isScrolled ? "bg-white/80" : "bg-white"
       }`}
     >
@@ -47,33 +47,38 @@ const Navbar = () => {
         onClick={onClickHandle}
       />
       <ul
-        className={`block absolute h-[270px] z-50 top-20 left-0 w-full bg-white lg:bg-transparent ${
+        className={`block absolute h-full z-50 top-20 left-0 w-full bg-white lg:bg-transparent ${
           isNavbarOpen ? "translate-x-0" : "-translate-x-[1000px]"
         } md:translate-x-0 text-center md:h-auto md:static md:w-fit md:gap-6 md:flex transition-all ease-in-out duration-500`}
         onClick={onClickHandle}
       >
-        <li className="menu font-semibold text-md mb-5 sm:mb-0 mt-5 sm:mt-0 relative after:contents-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[1px] after:bg-black after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100 after:origin-right hover:after:origin-left">
-          <a href="#about" className="menu-item">
+        <li className="relative menu font-semibold text-md mb-5 sm:mb-0 mt-5 z-50 sm:mt-0 hover:bg-slate-400/15 py-1 px-2 rounded-md overflow-hidden">
+          <a href="#home" className="menu-item" data-text="Home">
+            Home
+          </a>
+        </li>
+        <li className="relative menu font-semibold text-md mb-5 sm:mb-0 mt-5 sm:mt-0 hover:bg-slate-400/15 py-1 px-2 rounded-md overflow-hidden">
+          <a href="#about" className="menu-item" data-text="About">
             About
           </a>
         </li>
-        <li className="relative font-semibold text-md mb-5 sm:mb-0 after:contents-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[1px] after:bg-black after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100 after:origin-right hover:after:origin-left">
-          <a href="#projects" className="menu-item">
+        <li className="relative menu font-semibold text-md mb-5 sm:mb-0 hover:bg-slate-400/15 py-1 px-2 rounded-md overflow-hidden">
+          <a href="#projects" className="menu-item" data-text="Projects">
             Projects
           </a>
         </li>
-        <li className="relative font-semibold text-md mb-5 sm:mb-0 after:contents-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[1px] after:bg-black after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100 after:origin-right hover:after:origin-left">
-          <a href="#education" className="menu-item">
+        <li className="relative menu font-semibold text-md mb-5 sm:mb-0 hover:bg-slate-400/15 py-1 px-2 rounded-md overflow-hidden">
+          <a href="#education" className="menu-item" data-text="Education">
             Education
           </a>
         </li>
-        <li className="relative font-semibold text-md mb-5 sm:mb-0 after:contents-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[1px] after:bg-black after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100 after:origin-right hover:after:origin-left">
-          <a href="#certification" className="menu-item">
+        <li className="relative menu font-semibold text-md mb-5 sm:mb-0 hover:bg-slate-400/15 py-1 px-2 rounded-md overflow-hidden">
+          <a href="#certification" className="menu-item" data-text="Certification">
             Certification
           </a>
         </li>
-        <li className="relative font-semibold text-md mb-5 sm:mb-0 after:contents-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[1px] after:bg-black after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100 after:origin-right hover:after:origin-left">
-          <a href="#contact" className="menu-item">
+        <li className="relative menu font-semibold text-md mb-5 sm:mb-0 hover:bg-slate-400/15 py-1 px-2 rounded-md overflow-hidden">
+          <a href="#contact" className="menu-item" data-text="Contact">
             Contact
           </a>
         </li>
